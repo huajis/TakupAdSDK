60009542c9cb500f7939a0e3a34371359f57e384
huajis <59719343+huajis@users.noreply.github.com>
2026-04-05 11:33:01 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@
 
 import PackageDescription
 
+let version = "1.0.0"
+let checksum = "374d295d99389fa3e5c3c91d4bf32613236cfcaff133a984da77d22ad0d46fbc"
+
 let package = Package(
     name: "TakupAdSDK",
     platforms: [
@@ -11,14 +14,15 @@ let package = Package(
     products: [
         .library(
             name: "TakupAdSDK",
-            targets: ["TakupAdSDK"]
+            targets: ["TakupAdSDKXCFramework"]
         ),
     ],
     dependencies: [],
     targets: [
-        .target(
-            name: "TakupAdSDK",
-            path: "Sources/TakupAdSDK"
+        .binaryTarget(
+            name: "TakupAdSDKXCFramework",
+            url: "https://github.com/huajis/TakupAdSDK/releases/download/\(version)/TakupAdSDK.xcframework.zip"
+            checksum: checksum
         ),
     ]
 )
